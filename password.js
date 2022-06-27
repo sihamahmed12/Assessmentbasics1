46c433fe13e0d3827d44bc842d176f7f0f7c9d2e
sihamahmed12 <siham0992@gmail.com>
2022-06-26 22:18:36 -0500

    AssessmentIncludingPassword

--- password.js
+++ password.js
@@ -1,16 +1,17 @@
-let password = 'Hennepin2022'
-
-// let passWord = password.split('')
+let password = 'Hennepin2012'
 
 let isNum = /\d/.test(password)
 let isLetter = /[a-zA-z]/.test(password) 
-password != "password"
+let length = password.length 
+let upperCase = /[A-Z]/.test(password)
+password != "password" , "letmein" 
 
 // console.log(isNum) 
 // console.log(isLetter) 
  //console.log (length)
+//console.log(upperCase)
   
-  if ((password.length >= 10) && isNum === true && isLetter === true)  {
+  if ((password.length >= 10) && isNum === true && isLetter === true && upperCase === true)  {
     console.log("Sucess")
   }  else {
     console.log("Fail")
